b4183ec9fce7edaaa6542250d10ce5a101381436
Michal Bandyszak <michalbandyszak@gmail.com>
2018-07-15 22:27:43 +0200

    first commit

---
+++ components/App.js
@@ -0,0 +1,45 @@
+
+var contacts = [
+	{
+		id: 1,
+		firstName: 'Jan',
+		lastName: 'Nowak',
+		email: 'jan.nowak@example.com',
+	},
+
+	{
+		id: 2,
+		firstName: 'Adam',
+		lastName: 'Kowalski',
+		email: 'jk.kowalski@example.com',
+	},
+
+	{
+		id: 3,
+		firstName: 'Kamil',
+		lastName: 'Baszak',
+		email: 'kbaszak@example.com',
+		}
+];
+
+var contactForm = {
+	firstName: '',
+	lastName: '',
+	email: ''
+};
+
+
+
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div className = {'App'}>
+				<ContactForm contact = {contactForm}/>
+				<Contacts item = {contacts}/>		
+			</div>		
+
+			)
+		
+	}
+});
