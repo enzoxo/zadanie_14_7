14c02d75aa9aaac8cf5837a69912b1f8b643a5e4
Michal Bandyszak <michalbandyszak@gmail.com>
2018-07-17 19:59:52 +0200

    v1.1

--- components/Contact.js
+++ components/Contact.js
@@ -1,23 +1,24 @@
-var Contact= React.createClass({
+ var Contact= React.createClass({
 	propTypes: {
 		item: React.PropTypes.object.isRequired,
 	},
 
-	render: function() {
-		return (
-			<div className = {'contactItem'}>
-			<img className = {'contactImage'} src={'http://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/contacts-icon.png'}/>
-				
-				<p className = {'contactLabel'}>
-				 	Imię: {this.props.item.firstName}			
-				</p>
-				<p className = {'contactLabel'}>
-					Nazwisko: {this.props.item.lastName}
-				</p>
-				<a className = {'contactEmail'} href = {'mailto: '}>
-					email: {this.props.item.email}
-				</a>	
+	 render: function() {
+      return (
+          <div className={'contactItem'}>
+              <img className={'contactImage'} src={'http://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/contacts-icon.png'}/>
+              <p className={'contactLabel'}>
+                  Imię: {this.props.item.firstName}
+              </p>
+              <p className={'contactLabel'}>
+                  Nazwisko: {this.props.item.lastName}
+              </p>
+              <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                  {this.props.item.email}
+              </a>
+          </div>
+      )
+    },
+  });
 
-			}
-</div>
-})
+	
